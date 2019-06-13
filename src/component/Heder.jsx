86815d21bb52114a9355809ba207b5dff828b0ee
Amik1991 <amikamikamik1991@gmail.com>
2019-06-14 00:53:02 +0400

    add css module

--- src/component/Heder.jsx
+++ src/component/Heder.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import App from "../App";
-
+import s from'./Heder.module.css'
 const Heder =()=>{
 return(
-<heder className='heder'>
+<heder className={s.heder}>
     <img src='https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg'/>
 </heder>)}
 export default Heder;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import s from './Navbar.module.css'
 const Navbar =()=>{
     return(
-<nav className = 'nav' >
-    <div>Profile</div>
-    <div>Messages</div>
-    <div>News</div>
-    <div>Music</div>
-    <div>Settings</div>
+<nav className = {s.nav} >
+    <div className={s.item}>Profile</div>
+    <div className= {'${s.item} ${s.active}'}><a>Messages</a></div>
+    <div className={s.item}>News</div>
+    <div className={s.item}>Music</div>
+    <div className={s.item}>Settings</div>
 </nav>)}
 export default Navbar;
